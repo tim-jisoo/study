716b29573647d8ecade834553b4f9e9c682fa86b
jisoo heo <tim.jisoo.kor@gmail.com>
2018-07-05 01:30:28 +0900

    third commit

--- Algorithm/DynamicProgramming/Floyd/floyd.cpp
+++ Algorithm/DynamicProgramming/Floyd/floyd.cpp
@@ -8,108 +8,67 @@ using namespace std;
 class Graph
 {
 private:
-	bool flag;
 	int nNode;
 	int** table;
 	int** path;
 
 public:
-	void init()
-	{
-		for(int i = 1; i <= this->nNode; i++)
-		{
-			if(this->table[i])
-				delete this->table[i];
-			if(this->path[i])
-				delete this->path[i];
-		}
 
-		if(this->table)
-			delete this->table;
-		if(this->path)
-			delete this->path;
-		this->nNode = 0;
-		this->flag = false;
-	}
-
-	Graph(int n, int** w):nNode(n), flag(false)
+	Graph(int n, int** w):nNode(n)
 	{
-		this->table = new int*[this->nNode+1];
+		cout << "constructor called" << endl;
+	
+		this->table = w;
 		this->path = new int*[this->nNode+1];
 		for(int i = 1; i <= this->nNode; i++)
 		{
-			this->table[i] = new int[this->nNode+1];
-			for(int j = 1; j <= this->nNode; j++)
-				this->table[i][j] = w[i][j];
-			
 			this->path[i] = new int[this->nNode+1];
 			memset(this->path[i], 0x00, sizeof(int) * (this->nNode+1));
 		}
 	}
-
-	Graph(const Graph& copy):nNode(copy.nNode), flag(copy.flag)
-	{
-		this->init();		
-
-		this->table = new int*[this->nNode+1];
-		this->path = new int*[this->nNode+1];
-		for(int i = 1; i <= this->nNode; i++)
-		{
-			this->table[i] = new int[this->nNode+1];
-			for(int j = 1; j <= this->nNode; j++)
-				this->table[i][j] = copy.table[i][j];
-
-			this->path[i] = new int[this->nNode+1];
-			memset(this->path[i], 0x00, sizeof(int) * (this->nNode+1));
-		}
-
-	}
-
+	
 	~Graph()
 	{
-		this->init();
+		cout << "destructor called ..." << endl;
+
+		for(int i = 1; i <= this->nNode; i++)
+		{
+			if(this->table[i]) delete this->table[i];
+			if(this->path[i]) delete this->path[i];
+		}
+
+		if(this->table) delete this->table;
+		if(this->path) delete this->path;
 	}
 	
 	void floyd()
 	{
 		int i, j, k;
 
-		if(this->flag)
-		{
-			cout << "already knows the shortest path in this graph." << endl;
-			return;
-		}
-
-		this->flag = true;
-
-		//D^(k)[i][j] is shortest path for going to v_i to v_j by only passing the node v_1, v_2, ..., v_k
+		//D^(k)[i][j] is shortest path for going to v_i to v_j
+		//by only passing the node v_1, v_2, ..., v_k
 		for(k=1; k<=this->nNode; k++)
+		{
 			for(i=1; i<=this->nNode; i++)
 				for(j=1; j<=this->nNode; j++)
 					//D^(k)[i][j] = argmin(D^(k-1)[i][j], D^(k-1)[i][k]+D^(k-1)[k][j])
-					//When there are intermediate nodes in path for v_i to v_j, P[i][j] is most beggest index of node in those.
+					//When there are intermediate nodes in path for v_i to v_j,
+					//P[i][j] is most beggest index of node in those.
 					if(this->table[i][j] > this->table[i][k] + this->table[k][j])
 					{
 						this->table[i][j] = this->table[i][k] + this->table[k][j];
 						this->path[i][j] = k;
 					}
-
+		}
 	}
-
+	
 	void shortest_path(int i, int j)
 	{
-		if(!this->flag)
-		{
-			cout << "don't know the shortest path yet." << endl;
-			return;
-		}
-		
-		if(this->path[i][j])
-		{
-			shortest_path(i, this->path[i][j]);
-			cout << "v" << this->path[i][j] << ' ';
-			shortest_path(this->path[i][j], j);
-		}
+		if(!this->path[i][j]) return;
+	
+		shortest_path(i, this->path[i][j]);
+		cout << "v" << this->path[i][j] << ' ';
+		shortest_path(this->path[i][j], j);
 	}
 };
 
@@ -122,38 +81,36 @@ int read_arg_file(char** argv, Graph** grp)
 	
 	if((fp = fopen(argv[1], "rt")) == NULL) return -1;
 
+	//allocate table and path
 	memset(line, 0x00, sizeof(line));
 	fgets(line, sizeof(line), fp);
 	line[strlen(line)-1] = '\0';
-	fprintf(stdout, "num_node: %d\n", nNode = atoi(line));
-
+	cout << "n: " << (nNode = atoi(line)) << endl;
 	W = new int*[nNode+1];
 	for(i = 1; i <= nNode; i++)
 	{
 		W[i] = new int[nNode+1];
 		for(j = 1; j <= nNode; j++)
 		{
-			if(i==j) W[i][j] = 0;
-			else W[i][j] = 0x7FFFFFFF>>1;
+			if(i==j) W[i][j] = 0x00000000;
+			else W[i][j] = 0x3FFFFFFF;
 		}
 	}
 
+	//init table
 	for(i=1; i<=nNode; i++)
 	{
 		memset(line, 0x00, sizeof(line));
 		fgets(line, sizeof(line), fp);
 		line[strlen(line)-1] = '\0';
-
+		cout << "[" << i << "] ";
 		ptr = strtok(line, " ");
 		while(ptr != NULL)
 		{
 			len = strlen(ptr);
-			for(j=0; j<len; j++) if(ptr[j] == ':') break;
-			ptr[j] = '\0';
-			idx = atoi(ptr);
-			val = atoi(ptr+j+1);
-			W[i][idx] = val;
-			cout << idx << ':' << val << " ";
+			for(j=0; j<len; j++) if(ptr[j] == ':') { ptr[j] = '\0'; break; }
+			W[i][idx=atoi(ptr)] = (val = atoi(ptr+j+1));
+			cout << idx << ':' << val << ' ';
 			ptr = strtok(NULL, " ");
 		}
 		cout << endl;
@@ -162,9 +119,6 @@ int read_arg_file(char** argv, Graph** grp)
 	*grp = new Graph(nNode, W);	
 	
 	fclose(fp);
-
-	for(i = 1; i <= nNode; i++) if(W[i]) delete W[i];
-	if(W) delete W;
 
 	return 0;
 }
@@ -193,6 +147,8 @@ int main(int argc, char* argv[])
 	
 	grp->shortest_path(start, destination);
 	cout << endl;
+
+	delete grp;
 	return 0;
 }
 
